010134fc28d569cd632258d5cf401574eb515dde
hearthach <mplandauro@gmail.com>
2023-07-30 17:47:16 -0500

    subiendo primera version apiventa-v1

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    DEV_API: 'http://localhost:3000',
+    PRODUCTION_API: 'http://localhost:3000',
+    // PRODUCTION_API: 'https://api-dev-minimal-v510.vercel.app',
+  },
+};
---
+++ src/_mock/_auth.ts
@@ -0,0 +1,27 @@
+// _mock
+import { _mock } from 'src/_mock';
+
+// ----------------------------------------------------------------------
+
+export const JWT_SECRET = 'minimal-secret-key';
+
+export const JWT_EXPIRES_IN = '3 days';
+
+export const _users = [
+  {
+    id: '8864c717-587d-472a-929a-8e5f298024da-0',
+    displayName: 'Jonathan Rojas',
+    email: 'zilex@test.com',
+    password: 'test1234',
+    photoURL: _mock.image.avatar(24),
+    phoneNumber: '918039021',
+    country: 'Perú',
+    address: 'Lima, Ate-Salamanca',
+    state: 'Lima',
+    city: 'Ate',
+    zipCode: '051',
+    about: 'Lider de TI',
+    role: 'admin',
+    isPublic: true,
+  },
+];
